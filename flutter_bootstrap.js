1c16b7f72a0d89f40e9a25041d5ed796998232f1
Vivek <vivek@Viveks-Mac-mini.local>
2025-11-04 19:18:33 +0530

    v1.0.0[68]

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1727691520"
+    serviceWorkerVersion: "4217813818"
   }
 });
